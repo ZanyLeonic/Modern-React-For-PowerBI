cffaf9aefe7dd1613744863d8f8237c0f6f525d1
Leo Durrant <t-leodurrant@microsoft.com>
2024-01-16 17:19:29 +0000

    feat: moved TenantId and ClientId for MSAL to reference env vars

--- App/AuthConfig.ts
+++ App/AuthConfig.ts
@@ -1,8 +1,8 @@
 import { Configuration, PopupRequest } from "@azure/msal-browser";
 
 // Config Azure AD app setting to be passed to Msal on creation
-const TenantId = "11111111-1111-1111-1111-111111111111";
-const ClientId = "22222222-2222-2222-2222-222222222222";
+const TenantId = process.env.TenantId;
+const ClientId = process.env.ClientId;
 
 export const msalConfig: Configuration = {
     auth: {
